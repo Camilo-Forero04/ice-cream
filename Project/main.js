7009a8e351eb06749cc6dc27bc2cee65cf566b2d
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2024-02-12 22:25:28 -0500

    callback hell understood

--- Project/main.js
+++ Project/main.js
@@ -1,3 +1,4 @@
+//CALLBACK HELL
 let stocks ={
     Fruits :["strawberry", "grapes","banana","apple"],
     liquid:["water","ice"],
@@ -11,6 +12,31 @@ let order = (fruit_name,call_production)=>{
     call_production();
 };
 let production =()=>{
+    setTimeout(()=>{
+        console.log("production has started");
+        setTimeout(()=>{
+            console.log("the fruit has been chopped");
 
+            setTimeout(()=>{
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+
+                setTimeout(()=>{
+                    console.log("the machine was started");
+
+                    setTimeout(()=>{
+                        console.log(`ice cream was placed on a ${stocks.holder[1]}`);
+
+                        setTimeout(()=>{
+                            console.log(`${stocks.toppings[0]}was added as toppings`);
+
+                            setTimeout(()=>{
+                                console.log("serve ice cream")
+                            },2000)
+                        },3000)
+                    },2000)
+                },1000)
+            },1000);
+        },2000);
+    },0);
 };
 order(0,production);
